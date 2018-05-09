4f38e323f20a671638ef4fd5eb0ed06f7e001ad9
wuzuqing <1052243597@qq.com>
2018-05-09 13:42:08 +0800

    更新资源文件

---
+++ src/main/java/com/neeson/example/dto/FriendDTO.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: FriendDTO
+ * Author:   ChiMon
+ * Date:     2018/5/9 11:47
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+package com.neeson.example.dto;
+
+import lombok.Data;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author ChiMon
+ * @create 2018/5/9
+ * @since 1.0.0
+ */
+
+@Data
+public class FriendDTO {
+
+    private Integer userId;
+    private String userName;
+    private String userImg;
+}
---
+++ src/main/java/com/neeson/example/im/IMService.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: IMService
+ * Author:   ChiMon
+ * Date:     2018/5/9 11:41
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+package com.neeson.example.im;
+
+import com.neeson.example.dto.FriendDTO;
+
+import java.util.List;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author ChiMon
+ * @create 2018/5/9
+ * @since 1.0.0
+ */
+public interface IMService {
+
+    List<FriendDTO> getFriendList(String id);
+}
