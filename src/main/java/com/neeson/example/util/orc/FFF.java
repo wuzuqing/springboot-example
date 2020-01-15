d89117d6848c335892a56547a4ce3a018e0405aa
shiyuan <wuzuqing@linghit.com>
2020-01-15 09:37:43 +0800

    test

--- src/main/java/com/neeson/example/util/orc/FFF.java
+++ src/main/java/com/neeson/example/util/orc/FFF.java
@@ -1,55 +1,79 @@
-//package com.neeson.example.util.orc;
-//
-//import org.opencv.core.*;
-//import org.opencv.features2d.DescriptorMatcher;
-//import org.opencv.imgcodecs.Imgcodecs;
-//import org.opencv.imgproc.Imgproc;
-//import org.opencv.features2d.*;
-//public class FFF {
-//
-//    public void imgMatching2() throws Exception {
-//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-//        Mat src_base = Imgcodecs.imread("D:\\test\\test5.jpg");
-//        Mat src_test = Imgcodecs.imread("D:\\test\\test3.jpg");
+package com.neeson.example.util.orc;
+
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+
+import java.awt.*;
+import java.io.File;
+
+public class FFF {
+
+    public static void imgMatching2()  {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//        String name = "yamen_choose";
+//        Mat src_base = Imgcodecs.imread("D:\\"+name+".png");
 //        Mat gray_base = new Mat();
-//        Mat gray_test = new Mat();
 //        // 转换为灰度
-//        Imgproc.cvtColor(src_base, gray_base, Imgproc.COLOR_RGB2GRAY);
-//        Imgproc.cvtColor(src_test, gray_test, Imgproc.COLOR_RGB2GRAY);
-//        // 初始化ORB检测描述子
-//        FeatureDetector featureDetector = FastFeatureDetector.create(FastFeatureDetector.ORB);//特别提示下这里opencv暂时不支持SIFT、SURF检测方法，这个好像是opencv(windows) java版的一个bug,本人在这里被坑了好久。
-//        DescriptorExtractor descriptorExtractor = DescriptorExtractor.create(DescriptorExtractor.ORB);
-//        // 关键点及特征描述矩阵声明
-//        MatOfKeyPoint keyPoint1 = new MatOfKeyPoint(), keyPoint2 = new MatOfKeyPoint();
-//        Mat descriptorMat1 = new Mat(), descriptorMat2 = new Mat();
-//        // 计算ORB特征关键点
-//        featureDetector.detect(gray_base, keyPoint1);
-//        featureDetector.detect(gray_test, keyPoint2);
-//        // 计算ORB特征描述矩阵
-//        descriptorExtractor.compute(gray_base, keyPoint1, descriptorMat1);
-//        descriptorExtractor.compute(gray_test, keyPoint2, descriptorMat2);
-//        float result = 0;
-//        // 特征点匹配
-//        System.out.println("test5：" + keyPoint1.size());
-//        System.out.println("test3：" + keyPoint2.size());
-//        if (!keyPoint1.size().empty() && !keyPoint2.size().empty()) {
-//            // FlannBasedMatcher matcher = new FlannBasedMatcher();
-//            DescriptorMatcher matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE_L1);
-//            MatOfDMatch matches = new MatOfDMatch();
-//            matcher.match(descriptorMat1, descriptorMat2, matches);
-//            // 最优匹配判断
-//            double minDist = 100;
-//            DMatch[] dMatchs = matches.toArray();
-//            int num = 0;
-//            for (int i = 0; i < dMatchs.length; i++) {
-//                if (dMatchs[i].distance <= 2 * minDist) {
-//                    result += dMatchs[i].distance * dMatchs[i].distance;
-//                    num++;
-//                }
-//            }
-//            // 匹配度计算
-//            result /= num;
-//        }
-//        System.out.println(result);
-//    }
-//}
+////        Imgproc.cvtColor(src_base, gray_base, Imgproc.COLOR_RGB2GRAY);//342  614
+//        // D:\cut\half  // 288 335
+//        Mat cut1 = new Mat(src_base,new Rect(54,328,288,335));
+//        Mat cut2 = new Mat(src_base,new Rect(403,328,288,335));
+//        Mat cut3 = new Mat(src_base,new Rect(755,328,288,335));
+////        Mat cut1 = new Mat(src_base,new Rect(36,323,321,346));
+////        Mat cut2 = new Mat(src_base,new Rect(385,323,321,346));
+////        Mat cut3 = new Mat(src_base,new Rect(737,323,321,346));
+//        // 210 30
+//        Imgcodecs.imwrite("D:\\"+name+"1.jpg",cut1);
+//        Imgcodecs.imwrite("D:\\"+name+"2.jpg",cut2);
+//        Imgcodecs.imwrite("D:\\"+name+"3.jpg",cut3);
+        // 288 335
+       //229 266
+
+        /**
+         *    private String [] colors = {
+         *         "#4D2210",//空
+         *         "#363636",//白板
+         *         "#344931",//绿色 男爵
+         *         "#363636",
+         *         "#C1861C", //黄1 #C2871C
+         *         "#363636",
+         *         "#E48A19",// 黄2 #E38818  #E58B1A
+         *         "#363636"
+         *
+         *     };
+         */
+        int x = 186,y = 31;
+        File file = new File("D:\\cut\\cardBg");
+        File[] files = file.listFiles();
+        int index = 0;
+        for (File temp : files) {
+            if (temp.isFile()){
+                Mat tempMat = Imgcodecs.imread(temp.getAbsolutePath());
+                if (index==5){
+                    System.out.println(tempMat.rows() + "/"+tempMat.cols());
+                    Mat clone = tempMat.clone();
+                    clone.put(y,x, 0d,0d,255d);
+                    Imgcodecs.imwrite("D:\\clone1.jpg",clone);
+                }
+                index++;
+                // 30 250
+                double[] doubles = tempMat.get(y, x);
+                if (doubles!=null){
+                    Color color = new Color((int) doubles[2], (int) doubles[1], (int) doubles[0], 255);
+                    String hexString = Integer.toHexString(color(color));
+                    System.out.println(temp.getName() + " : #"+  hexString);
+                }
+
+            }
+        }
+    }
+
+    private static int color(Color color){
+        return (color.getAlpha() << 24) | (color.getRed() << 16) | (color.getGreen() << 8) | color.getBlue();
+    }
+
+
+    public static void main(String[] args) {
+        imgMatching2();
+    }
+}
